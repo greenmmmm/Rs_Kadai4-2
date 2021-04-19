25920d11ffdab81de2b3a4e424f0cab6954ee93b
github greenmmmm <green_apple_chair_card392@yahoo.co.jp>
2021-04-19 21:42:01 +0900

    検索メソッド記載

--- src/main/java/com/example/demo/mapper/OkashiMapper.java
+++ src/main/java/com/example/demo/mapper/OkashiMapper.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.domain.Okashi;
+import com.example.demo.form.SearchForm;
+
 
 //MyBatisでリポジトリを作成する
 @Mapper
 public interface OkashiMapper {
-	//全件取得。メソッド処理内容は、xmlファイルで記載
-	List<Okashi> selectAll();
+	//フォームに入った値を検索。メソッド処理内容は、xmlファイルで記載
+	List<Okashi> findBysearchForm(SearchForm searchForm);
 }
